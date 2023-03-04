ecf73af69a85bf5c3228ef3564ef55b01bd53451
terakura.aina <aicaierwei@gmail.com>
2023-03-04 20:22:40 +0900

    cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # originsはいくつでも設定できる ex: 'https://example.jp', 'http://localhost:3000'
-    origins "localhost:3000" # React側はポート番号3000で作るので「localhost:3000」を指定
+    # "localhost:3000" # React側はポート番号3000で作るので「localhost:3000」を指定
+    # TODO: vercelがデプロイごとにURLが変わるのでどのように対処するのか調べてドメイン設定したい
+    origins "*"
 
     resource "*",
       headers: :any,
